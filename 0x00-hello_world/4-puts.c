2eecdd618825d071881b5cc9f977f472ebe01582
akonuche <luischuks4me@gmail.com>
2022-09-09 00:35:17 +0100

    love coding

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,12 @@
 #include <stdio.h>
 
 /**
- *
  * main - Printing successful
- *
- * Return: Always 0  (successful printing)
+ * Return: 0 if exited rightly
  */
 int main(void)
 {
-
-	puts ("\"programming is like building a multilingual puzzle");
+	puts("\"programming is like building a multilingual puzzle");
 	return (0);
 }
 
