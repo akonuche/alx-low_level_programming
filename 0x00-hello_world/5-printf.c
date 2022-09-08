0fdd90eea0eb55b42699b32e26046e6714caaa3c
akonuche <luischuks4me@gmail.com>
2022-09-09 00:42:33 +0100

    love coding

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /**
  * main -prints a code lover
  *
@@ -6,5 +7,6 @@
  */
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return(0);
+
+	return (0);
 }
