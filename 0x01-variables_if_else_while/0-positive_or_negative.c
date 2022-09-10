31536379d550ae22490ce39473ec2592172665ac
akonuche <luischuks4me@gmail.com>
2022-09-10 04:54:00 +0100

    love coding

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,13 +4,14 @@
 
 /**
  * main - assigns a random number to int n everytime
- * Return: Always 0 
+ * Return: Always 0 success
  */
 int main(void)
 {
 	int n;
 
 	srand(time(0));
+
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
 		printf("%d is positive\n", n);
