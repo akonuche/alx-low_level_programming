ac438e99758ac0bfa0d9ac1398288a7243b75cd8
akonuche <luischuks4me@gmail.com>
2022-09-15 05:22:18 +0100

    coding

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -7,6 +7,7 @@
 void print_times_table(int n)
 {
 	int i, j, k;
+
 	if (n >= 0 && n <= 15)
 	{
 		for (i = 0; i <= n; i++)
@@ -17,7 +18,7 @@ void print_times_table(int n)
 				if (j == 0)
 				{
 					_putchar(k + '0');
-				} else if (k < 10 && j != )
+				} else if (k < 10 && j != 0)
 				{
 					_putchar(',');
 					_putchar(' ');
