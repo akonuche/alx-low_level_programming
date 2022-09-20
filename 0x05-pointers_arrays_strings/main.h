29fb1dc1b7855931d59c5cbe8ad9ba7e0ca8ba89
akonuche <luischuks4me@gmail.com>
2022-09-21 00:12:06 +0100

    coder rules

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,12 +1,17 @@
-#include "main.h"
+#ifndef FILE_MAIN
+#define FILE_MAIN
 
-/**
- * main - list of project
- * Return: 0
- */
-int main(void)
-{
-	0-reset_to_98.c;
-	1-swap.c;
-	2-strlen.c;
-}
+int _putchar(char c);
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+int _strlen(char *s);
+void _puts(char *str);
+void print_rev(char *s);
+void rev_string(char *s);
+void puts2(char *str);
+void puts_half(char *str);
+void print_array(int *a, int *n);
+char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
+
+#endif
