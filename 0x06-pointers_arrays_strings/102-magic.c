d180b00fc3337db5ae3f047d2c562113f5462571
akonuche <luischuks4me@gmail.com>
2022-09-24 02:15:41 +0100

    love thee

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -9,7 +9,7 @@ int main(void)
 	a[2] = 1024;
 	p = &n;
 	/**
-	 * write your line of the code here
+	 * write your line of the code here...
 	 * remember:
 	 * - you are not allowed to use a
 	 * - you are not allowed to modify p
