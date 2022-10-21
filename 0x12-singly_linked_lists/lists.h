1118007c58ecfc2a423bfac95611954f887404fe
akonuche <luischuks4me@gmail.com>
2022-10-21 03:18:57 +0100

    lovers

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,5 +1,5 @@
 #ifndef LISTS_H
-#define LIST_H
+#define LISTS_H
 
 /**
  * struct list_s - singly linked list
@@ -8,15 +8,14 @@
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- * for akonuche project
+ * for Akonuche project
  */
-typed struct list_s
+typedef struct list_s
 {
 	char *str;
 	unsigned int len;
 	struct list_s *next;
 } list_t;
-
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
